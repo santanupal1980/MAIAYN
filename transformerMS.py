446df57cfa356c743c22ca38ba749abff2a1b532
Nico Herbig <nico.herbig@dfki.de>
2018-08-29 09:20:39 +0200

    fixed encoding bug for windows and bug in feedforward layer

--- transformerMS.py
+++ transformerMS.py
@@ -122,7 +122,7 @@ class MultiHeadAttention():
 
 class PositionwiseFeedForward():
     def __init__(self, d_hid, d_inner_hid, dropout=0.1):
-        self.w_1 = Conv1D(d_inner_hid, 1, activation='relu')
+        self.w_1 = Conv1D(4*d_inner_hid, 1, activation='relu')
         self.w_2 = Conv1D(d_hid, 1)
         self.layer_norm = LayerNormalization()
         self.dropout = Dropout(dropout)
